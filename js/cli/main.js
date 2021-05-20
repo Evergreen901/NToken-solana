a751247dec4864e706c6647888c87dcb874b3c94
Jawaher Korbosli <jawaher.korbosli@gmail.com>
2021-05-20 10:56:26 +0100

    first commit

---
+++ js/cli/main.js
@@ -0,0 +1,45 @@
+/**
+ * Exercises the token program
+ *
+ * @flow
+ */
+
+import {
+  loadTokenProgram,
+  createMint,
+  createAccount,
+  transfer,
+  mintTo,
+  runDeposit,
+  withDraw,
+  runGetFullBalance,
+  runApproveChecked
+} from './token-test';
+
+async function main() {
+  console.log("********** NoVa FINaNCe NTOKEN PROJECT **********");
+  console.log('Run test: loadTokenProgram');
+  await loadTokenProgram();
+  console.log('Run test: createMint');
+  await createMint();
+  console.log("Run test: createAccount");
+  await createAccount();
+  console.log('Run test: mintTo');
+  await mintTo();
+  console.log("Run test: approveChecked");
+  await runApproveChecked();
+  console.log("Run test: runDeposit");
+  await runDeposit();
+ // console.log('Run test: transfer');
+  // await transfer();
+  console.log('Run test: withDraw');
+  await withDraw();
+  console.log( " *************** Finish *************** ");
+}
+
+main()
+  .catch(err => {
+    console.error(err);
+    process.exit(-1);
+  })
+  .then(() => process.exit());
